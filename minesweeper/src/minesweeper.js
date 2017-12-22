1d82d7a58fee3bfae20920de40720d191c95e54b
Kyle Hunter <kyle.hunter91@gmail.com>
2017-12-22 14:18:13 -0800

    Beginning to move away from a hardcoded board. Created a function to print the board

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,18 +1,27 @@
-// hardcoded one row with three columns
-const blankLine = '  |   |  ';
-
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-
-// represents what the board will look like when the player guesses by clicking the first square of this row
-const guessLine = '1 |   |  ';
-// represents what the board will look like when the player clicks and reveals a bomb
-const bombLine = '  | B |  ';
+// function to format the printed board output
+const printBoard = (board) => {
+  console.log(`Current board:`);
+  // adding the pipe to the board. join() joins all elements of an array and combines them
+  // console.log(board[0].join(' | '));
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
 
 
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+
+// generic game board
+let board = [[' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']];
+
+printBoard(board);
+
+console.log();
+console.log(`!!!Beginning to test the board`)
+console.log();
+
+// changes two values to contain a user guess and a bomb
+board[0][1] = 1;
+board[2][2] = 'B';
+printBoard(board);
