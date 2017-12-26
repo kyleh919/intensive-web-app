f85300522aefa25f47e3b6868573a5101ae5305a
Kyle Hunter <kyle.hunter91@gmail.com>
2017-12-26 15:49:14 -0500

    Rebuilt minesweeper.js to add functions to build player board, bomb board, and print boards

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,27 +1,92 @@
-// function to format the printed board output
-const printBoard = (board) => {
-  console.log(`Current board:`);
-  // adding the pipe to the board. join() joins all elements of an array and combines them
-  // console.log(board[0].join(' | '));
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+// Instead of creating one singular game board, we're going to create two: one
+//  board for the player's guesses and another board that will hold the actual
+//  bomb locations.
+
+
+// generates a players board of a given size to hold the player's guesses.
+//  accepts the number of rows and number of columns as parameters and sets the
+//  board accordingly
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+
+  for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+
+    for(let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+
+  return board;
 };
 
+// generates a bomb board based on the number of bombs passed in as a parameters
+//  accepts the number of rows and number of columns as parameters and sets the
+//  board accordingly
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  let numberOfBombsPlaced = 0;
+  let randomRowIndex = 0;
+  let randomColumnIndex = 0;
+
+  for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+
+    for(let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+
+  // console.log('\t***Debug*** ' + 'numberOfBombsPlaced ' + numberOfBombsPlaced + ' ' + 'numberOfBombs ' + numberOfBombs);
+
+  // generates bombs, the total number of bombs is based on a passed in parameter above
+  //  randomly generates the row,column of the bomb
+  while(numberOfBombsPlaced < numberOfBombs) {
+    // add logic to not duplicate spots of bombs after learning control flow
+    randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    // console.log('\t***Debug*** ' + 'row ' + randomRowIndex + ' ' + 'col ' + randomColumnIndex);
+
+    board[randomRowIndex][randomColumnIndex] = 'B';
 
 
-// generic game board
-let board = [[' ', ' ', ' '],
-            [' ', ' ', ' '],
-            [' ', ' ', ' ']];
+    numberOfBombsPlaced++;
+  }
 
-printBoard(board);
+  return board;
+};
 
-console.log();
-console.log(`!!!Beginning to test the board`)
-console.log();
+// prints board
+let printBoard = function(board) {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+}
 
-// changes two values to contain a user guess and a bomb
-board[0][1] = 1;
-board[2][2] = 'B';
-printBoard(board);
+
+let playerBoard = generatePlayerBoard(3,4);
+// console.log(playerBoard);
+
+let bombBoard = generateBombBoard(3,4,5);
+// console.log(bombBoard);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
+
+
+// console.log(printBoard(myBoard));
+
+
+
+
+// sanbox
+// let fun = [[null, null, null],
+//           [null, null, null]];
+// // let fun = [[' ', ' ', ' '],
+// //           [' ', ' ', ' ']];
+// console.log(fun);
+// fun[0][0] = 'hi';
+// // fun[0] = 'hi';
+// console.log(fun);
