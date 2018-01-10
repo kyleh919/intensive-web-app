5be8d1496f172092d23460ac7bf753ee9d860da7
Kyle Hunter <kyle.hunter91@gmail.com>
2018-01-09 23:28:31 -0800

    Introducing Ravenous project

---
+++ ravenous/src/components/BusinessList/BusinessList.js
@@ -0,0 +1,25 @@
+/* BusinessList.js */
+/*
+BusinessList component purpose: simulate what a returned list of businesses
+would look like in Ravenous (after querying the Yelp API).
+
+Components used:
+-Business
+*/
+
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business.js';
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    );
+  }
+};
---
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,10 @@
+/* SearchBar.js */
+/*
+SearchBar component purpose: communicate with the Yelp API.
+
+Should allow users to search businesses by: best match, highest rated, or most
+  reviewed.
+*/
+
+import React from 'react';
+import './SearchBar.css';
